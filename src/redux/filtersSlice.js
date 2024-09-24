7ca2d801f5e23fdbad8dada47582c1a9bac43ebb
Miami <alex.miami.petrov@gmail.com>
2024-09-24 12:56:08 +0300

    m

---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "filters",
+  initialState: {
+    name: "",
+  },
+});
+
+// const slice = createSlice({
+//   name: "filters",
+//   initialState: {
+//     status: "all",
+//   },
+//   reducers: {
+//     setStatusFilter(state, action) {
+//       return {
+//         ...state,
+//         status: action.payload,
+//       };
+//     },
+//   },
+// });
+
+// export const { setStatusFilter } = slice.actions;
+
+// export default slice.reducer;
